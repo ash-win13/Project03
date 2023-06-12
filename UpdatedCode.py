6884ce448468c613a00693f82af559c2e73bba02
Ashwin <126145993+ash-win13@users.noreply.github.com>
2023-06-13 00:46:33 +0530

    Add files via upload
    Here are the overall features added to the code:
    
    1) Trendline with Breaks: The code calculates a trendline using a specified period and identifies trendline breaks when the price crosses above or below the trendline.
    
    TRAMA (Trend Reversal Adaptive Moving Average): The code calculates the TRAMA value based on a specified period and uses it as an additional indicator.
    
    2) Buy and Sell Orders: The code includes functions to place buy and sell orders for a specified cryptocurrency and quantity. These orders are executed based on the trendline breaks and position management logic.
    
    3) Position Management: The code manages the position state to ensure that a buy order is placed only when there is a trendline break and no existing position, and a sell order is placed only when there is no trendline break and an existing position.
    
    4) Interactive Inputs: The code prompts the user to enter the cryptocurrency symbol, quantity to buy, and quantity to sell for each iteration of the trading loop. This allows for dynamic input based on user preferences.
    
    5) Bot Enable/Disable: The code includes a toggle_bot() function that enables or disables the bot's execution. When the bot is disabled, it skips the trading logic and waits for the next iteration.
    
    6) Binance API Integration: The code fetches candlestick data from the Binance exchange using the CCXT library. It uses this data to calculate indicators and make trading decisions.
    
    7) User Input for Cryptocurrency and Trade Quantity: The code allows the user to input the cryptocurrency symbol, quantity to buy, and quantity to sell. This provides flexibility for the user to choose the cryptocurrency they want to trade and specify the trade quantities.

---
+++ UpdatedCode.py
@@ -0,0 +1,83 @@
+import ccxt
+import time
+
+trendline_period = 20
+trama_period = 10
+
+trendline_break = False
+trama = []
+in_position = False
+is_enabled = True
+
+api_key = 'YOUR_API_KEY'
+api_secret = 'YOUR_API_SECRET'
+
+exchange = ccxt.binance({
+    'apiKey': api_key,
+    'secret': api_secret
+})
+
+def calculate_trendline_with_breaks(data):
+    trendline = sum(data[-trendline_period:]) / trendline_period
+
+    if data[-1] > trendline and data[-2] <= trendline:
+        return True
+    elif data[-1] < trendline and data[-2] >= trendline:
+        return True
+    else:
+        return False
+
+def calculate_trama(data):
+    if len(data) >= trama_period:
+        trama_sum = sum(data[-trama_period:])
+        trama_average = trama_sum / trama_period
+        return trama_average
+    else:
+        return None
+
+def place_buy_order(symbol, quantity):
+    print(f"Placing buy order for {quantity} {symbol}")
+
+def place_sell_order(symbol, quantity):
+    print(f"Placing sell order for {quantity} {symbol}")
+
+def toggle_bot():
+    global is_enabled
+    is_enabled = not is_enabled
+    print("Bot is", "enabled" if is_enabled else "disabled")
+
+def main_loop():
+    while True:
+        try:
+            if is_enabled:
+                symbol = input("Enter the symbol of the cryptocurrency (e.g., BTC/USDT): ")
+                buy_quantity = float(input("Enter the quantity to buy: "))
+                sell_quantity = float(input("Enter the quantity to sell: "))
+
+                candlesticks = exchange.fetch_ohlcv(symbol, '1m')
+                close_prices = [candlestick[4] for candlestick in candlesticks]
+
+                trendline_break = calculate_trendline_with_breaks(close_prices)
+
+                trama_value = calculate_trama(close_prices)
+                if trama_value is not None:
+                    trama.append(trama_value)
+
+                if trendline_break and not in_position:
+                    place_buy_order(symbol, buy_quantity)
+                    in_position = True
+                elif not trendline_break and in_position:
+                    place_sell_order(symbol, sell_quantity)
+                    in_position = False
+
+                print("Trendline Break:", trendline_break)
+                print("TRAMA:", trama)
+
+        except Exception as e:
+            print("An error occurred:", str(e))
+
+        time.sleep(60)
+
+toggle_bot()
+
+main_loop()
